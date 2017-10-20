8eb94fcb1eea5f4ba291df754f1f64314dc1858f
Scott Worley <sworley@taos.com>
2017-10-20 05:39:45 +0000

    Added licensing details as the majority of this script is currently leveraged from the example provided.

--- rsbp.py
+++ rsbp.py
@@ -8,7 +8,31 @@
 # 1.2 - 10.19.17 - sworley - Finally functionality !!! Reading From a RFID
 # 1.1 - 10.18.17 - sworley - Imported files into a real repository
 # 1.0 - 10.17.17 - sworley - Initially created the file based off of the 
-#                            Adafruit_Python_PN532 examples.
+#                            Adafruit_Python_PN532 mcpi_listen.py example.
+#                            Until the main contentubstantially decreases,
+#                            The licensing Foo will be included below ...
+# ----
+# Raspberry Pi Minecraft Block NFC Listener
+# Author: Tony DiCola
+# Copyright (c) 2015 Adafruit Industries
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 # ----
 # import-ant stuff ...
 import binascii                # Convert card H3XX details
